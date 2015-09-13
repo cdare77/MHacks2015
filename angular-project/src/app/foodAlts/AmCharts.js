61e974a2888af0f4af27aab3c0962cec76dec23a
Chris Dare <Chris@Chriss-MacBook-Pro-2.local>
2015-09-13 08:40:44 -0400

    finished AmCharts.js

--- angular-project/src/app/foodAlts/AmCharts.js
+++ angular-project/src/app/foodAlts/AmCharts.js
@@ -3,16 +3,17 @@ var chart = AmCharts.makeChart( "chartdiv", {
   "theme": "default",
   "dataProvider": [ {
     "title": "Low Risk",
-    "value": 100
+    "value": 10,
   }, {
     "title": "Medium Risk",
-    "value": 75
+    "value": 20,
   }, {
     "title": "High Risk",
-    "value": 50
+    "value": 30
+
   }, {
     "title": "Extreme Risk",
-    "value": 25
+    "value": 40,
   } ],
   "balloon": {
     "fixedPosition": true
@@ -29,7 +30,7 @@ var chart = AmCharts.makeChart( "chartdiv", {
       "url": "suggestions.html",
       "size": 40,
       "x": 475,
-      "y": 500,
+      "y": 590,
       "color": "#000000"
     },
     {
@@ -38,34 +39,110 @@ var chart = AmCharts.makeChart( "chartdiv", {
       "url": "suggestions.html",
       "bold": true,
       "x": 540,
-      "y": 275,
+      "y": 510,
       "color": "#000000"
     },
     {
       "text": "Medium Risk",
-      "size": 25,
+      "size": 22,
       "url": "suggestions.html",
       "bold": true,
       "x": 530,
-      "y": 140,
+      "y": 390,
       "color": "#000000",
     },
     {
       "text": "Low Risk",
-      "size": 18,
+      "size": 16,
       "url": "suggestions.html",
       "bold": true,
-      "x": 580,
-      "y": 45,
+      "x": 578,
+      "y": 200,
+      "color": "#000000"
+    },
+    {
+      "text": "Red meats and salty foods should be avoided",
+      "size": 14,
+      "x": 810,
+      "y": 600,
+      "color": "#000000"
+    },
+    {
+      "text": "Exercise regularly; primarily cardiovascular",
+      "size": 14,
+      "x": 760,
+      "y": 530,
+      "color": "#000000"
+    },
+    {
+      "text": "Fish and poultry contain low cholesterol and high protein",
+      "size": 14,
+      "x": 240,
+      "y": 550,
+      "color": "#000000"
+    },
+    {
+      "text": "Stick to tree nuts with low saturated fat",
+      "size": 14,
+      "x": 290,
+      "y": 430,
+      "color": "#000000"
+    },
+    {
+      "text": "Pick low sugar juices not made from concentrate",
+      "size": 14,
+      "x": 440,
+      "y": 330,
+      "color": "#000000"
+    },
+    {
+      "text": "Avoid empty calories such as lipids and beer",
+      "size": 14,
+      "x": 630,
+      "y": 430,
+      "color": "#000000"
+    },
+    {
+      "text": "Given the option, choose no salt",
+      "size": 14,
+      "x": 500,
+      "y": 250,
+      "color": "#000000"
+    },
+    {
+      "text": "Never underestimate",
+      "size": 14,
+      "x": 530,
+      "y": 150,
+      "color": "#000000"
+    },
+    {
+      "text": "the value of stretching",
+      "size": 14,
+      "x": 550,
+      "y": 170,
+      "color": "#000000"
+    },
+    {
+      "text": "Practice aerobics on a daily basis",
+      "size": 14,
+      "x": 140,
+      "y": 600,
       "color": "#000000"
     }
   ],
+  "titles": [{
+    "text": "Obesity Risk Factor",
+    "size": 25,
+    "color": "#FFCC00"
+  }],
   "rotate": true,
   "labelPosition": "right",
   "balloonText": "[[title]]: [[value]][[description]]",
   "export": {
     "enabled": true
   }
+  
 } );
 
 $( "#chartdiv" ).click(function() {
