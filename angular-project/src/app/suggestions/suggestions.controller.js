44c6d530eb23c99708a222511a8b0dfb8e71ab8d
swarage <swaraj.dhumne@gmail.com>
2015-09-13 11:52:11 -0400

    works with suggestions

--- angular-project/src/app/suggestions/suggestions.controller.js
+++ angular-project/src/app/suggestions/suggestions.controller.js
@@ -15,152 +15,77 @@
 
     vm.risk = $routeParams.risk;
 
-    vm.chart = AmCharts.makeChart( "chartdiv", {
-      "type": "funnel",
-      "theme": "default",
-      "dataProvider": [ {
-        "title": "Low Risk",
-        "value": 10
-      }, {
-        "title": "Medium Risk",
-        "value": 20
-      }, {
-        "title": "High Risk",
-        "value": 30
+    console.log(vm.risk);
 
-      }, {
-        "title": "Extreme Risk",
-        "value": 40
-      } ],
-      "balloon": {
-        "fixedPosition": true
-      },
-      "valueField": "value",
-      "titleField": "Obesity Risk Level",
-      "marginRight": 240,
-      "marginLeft": 50,
-      "startX": -500,
-      "allLabels": [
-        {
-          "text": "Extreme Risk",
-          "bold": true,
-          "url": "suggestions.html",
-          "size": 40,
-          "x": 475,
-          "y": 590,
-          "color": "#000000"
-        },
-        {
-          "text": "High Risk",
-          "size": 30,
-          "url": "suggestions.html",
-          "bold": true,
-          "x": 540,
-          "y": 510,
-          "color": "#000000"
-        },
-        {
-          "text": "Medium Risk",
-          "size": 22,
-          "url": "suggestions.html",
-          "bold": true,
-          "x": 530,
-          "y": 390,
-          "color": "#000000"
-        },
-        {
-          "text": "Low Risk",
-          "size": 16,
-          "url": "suggestions.html",
-          "bold": true,
-          "x": 578,
-          "y": 200,
-          "color": "#000000"
-        },
-        {
-          "text": "Red meats and salty foods should be avoided",
-          "size": 14,
-          "x": 810,
-          "y": 600,
-          "color": "#000000"
-        },
-        {
-          "text": "Exercise regularly; primarily cardiovascular",
-          "size": 14,
-          "x": 760,
-          "y": 530,
-          "color": "#000000"
-        },
-        {
-          "text": "Fish and poultry contain low cholesterol and high protein",
-          "size": 14,
-          "x": 240,
-          "y": 550,
-          "color": "#000000"
-        },
-        {
-          "text": "Stick to tree nuts with low saturated fat",
-          "size": 14,
-          "x": 290,
-          "y": 430,
-          "color": "#000000"
-        },
-        {
-          "text": "Pick low sugar juices not made from concentrate",
-          "size": 14,
-          "x": 440,
-          "y": 330,
-          "color": "#000000"
-        },
-        {
-          "text": "Avoid empty calories such as lipids and beer",
-          "size": 14,
-          "x": 630,
-          "y": 430,
-          "color": "#000000"
-        },
-        {
-          "text": "Given the option, choose no salt",
-          "size": 14,
-          "x": 500,
-          "y": 250,
-          "color": "#000000"
-        },
-        {
-          "text": "Never underestimate",
-          "size": 14,
-          "x": 530,
-          "y": 150,
-          "color": "#000000"
-        },
-        {
-          "text": "the value of stretching",
-          "size": 14,
-          "x": 550,
-          "y": 170,
-          "color": "#000000"
-        },
-        {
-          "text": "Practice aerobics on a daily basis",
-          "size": 14,
-          "x": 140,
-          "y": 600,
-          "color": "#000000"
+    function food(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    var tier4 = [new food("Vegetables", 0.0), new food("Whole Grain Bread", 0.0), 
+        new food("Skim Milk", 0.0), new food("Fruit", 0.0), new food("Lean Cuisine", 0.0)];
+
+    var tier3 = [new food("Fish", 0.0), new food("Low Fat Milk", 0.0), new food("Fruit", 0.0)];
+
+    var tier2 = [new food("Poultry", 0.0), new food("Nuts", 0.0), new food("White Bread", 0.0)];
+
+    var tier1 = [new food("Red Meat", 0.0), new food("Whole Milk", 0.0)];
+
+    var ajaxFinished = false;
+
+    function findPrice(food) {
+        jQuery.ajax( {
+            url: "http://api.walmartlabs.com/v1/search?query=" + food.name.replace(" ", "+") + "&format=json&apiKey=f9e8tfetpv3r64c6ewzjtkrt",
+            type: 'GET',
+            data: { content: 'testing test' },
+            beforeSend : function(xhr) {
+                xhr.setRequestHeader( "X-Originating-Ip", "35.0.122.37");
+            },
+            success: function( response ) {
+                ajaxFinished = true;
+                if (response.hasOwnProperty('items')) {
+                    console.log(food.name);
+                    console.log(response.items[0].name)
+                    console.log(response.items[0].salePrice);
+                    food.price = response.items[0].salePrice;
+                }
+                else {
+                    food.price = -1;
+                }
+            }
+        });
+    }
+
+    function findPrices(array){
+        for (var i = 0; i < array.length; i ++) {
+            findPrice(array[i]);
+            //setTimeout(200);
         }
-      ],
-      "titles": [{
-        "text": "Obesity Risk Factor",
-        "size": 25,
-        "color": "#FFCC00"
-      }],
-      "rotate": true,
-      "labelPosition": "right",
-      "balloonText": "[[title]]: [[value]][[description]]",
-      "export": {
-        "enabled": true
-      }
-      
-    } );
+    }
+
+    switch(vm.risk) {
+        case "Low Risk":
+            findPrices(tier1);
+            vm.items = tier1;
+            break;
+        case "Medium Risk":
+            findPrices(tier2);
+            vm.items = tier2;
+            break;
+        case "High Risk":
+            findPrices(tier3);
+            vm.items = tier3;
+            break;
+        case "Extreme Risk":
+            findPrices(tier4);
+            vm.items = tier4;
+            break;
+        default:
+            findPrices(tier2);
+            vm.items = tier2;
+    }
+
+    console.log(vm.items);
    
   }
 })();
